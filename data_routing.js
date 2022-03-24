48a1ae366a3f2cac577a5d282be5a8afd0ff4acb
lukasz-met <100130197+lukasz-met@users.noreply.github.com>
2022-03-24 12:16:53 +0000

    Update data_routing.js
    Reflection of various changes to the request form, up to and including v2.3

--- data_routing.js
+++ data_routing.js
@@ -11,18 +11,21 @@ task.short_description += " - " + current.variables.request.getDisplayValue();
 
 task.description = "Requested for:  " + current.variables.u_requested_for.name + "\n\n";
 
-if (current.variables.request == "add") {
-  task.short_description += " - " + current.variables.location;
-
-  if (current.variables.location == "external") {
-    task.description += "Username: " + current.variables.username + "\n\n";    
-  }
+if (current.variables.request == "add_amend") {
+  task.description += "DATA SOURCE\nFilename(s): " + current.variables.filename + "\n"
+  + "Source: " + current.variables.source + "\n"
+  + "Data collection: " + current.variables.data_collection.getDisplayValue() + "\n\n"
+  + "DATA DELIVERY/DESTINATION\nRouting Type: " + current.variables.routing_type.getDisplayValue() + "\n"
+  + "Destination: " + current.variables.destination + "\n"
+  + "Delivery: " + current.variables.delivery.getDisplayValue() + "\n"
+  + "Credentials: " + current.variables.credentials + "\n\n"
+  + "ABOUT DATA ROUTING\nDetails: " + current.variables.add_amend_details + "\n\n";
 }
 
-task.description += "Filename: " + current.variables.filename + "\n\n"
-+ "Source: " + current.variables.source + "\n\n"
-+ "Destination: " + current.variables.destination + "\n\n"
-+ "Date to be completed: " + current.variables.date.getDisplayValue() + "\n\n"
-+ "Priority: " + current.variables.priority.getDisplayValue() + "\n\n"
-+ "Details: " + current.variables.details + "\n\n"
-+ "Additional Information: " + current.variables.info;
+else {
+  task.description += "What to remove: " + current.variables.remove_what.getDisplayValue() + "\n"
+  + "Details: " + current.variables.remove_details + "\n\n";
+}
+
+task.description += "Date to be completed: " + current.variables.date.getDisplayValue() + "\n"
++ "Priority: " + current.variables.priority.getDisplayValue();
